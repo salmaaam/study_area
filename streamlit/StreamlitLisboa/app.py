10ad45653c63627be19436eb63c900b126462d22
Guilherme Irigon <99355325+Siri2191@users.noreply.github.com>
2023-03-06 09:25:40 -0300

    Add files via upload

--- streamlit/StreamlitLisboa/app.py
+++ streamlit/StreamlitLisboa/app.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 import pydeck as pdk
 import math
+import plotly.express as px
 
 url1='https://raw.githubusercontent.com/Siri2191/study_area/main/streamlit/lisbon_weekdays.csv'
 url2='https://raw.githubusercontent.com/Siri2191/study_area/main/streamlit/lisbon_weekends.csv'
@@ -54,7 +55,12 @@ capacidade_filter = st.sidebar.slider('Quantas pessoas são ao total?',1,8,1)
 
 fds_filter = st.sidebar.multiselect(label='Escolha se é fim de semana ou não', options=['dia de semana','fim de semana'])
 
-df_filtrado = df[(df['preco']>=preco_filter[0]) &(df['preco']<=preco_filter[1]) | (df['capacidade'] == float(capacidade_filter)) | (df['cat.semana'].isin(fds_filter))]
+# mensagem a quantidade de dado selecionados
+
+
+df_filtrado = df[(df['preco']>=preco_filter[0]) &(df['preco']<=preco_filter[1]) & (df['capacidade'] == float(capacidade_filter)) & (df['cat.semana'].isin(fds_filter))]
+
+st.sidebar.write('Existem {} imovéis na regiaão com estas caracteristicas'.format(len(df_filtrado)))
 
 #Ao lado da barra de filtro
 
@@ -75,3 +81,11 @@ st.pydeck_chart(pydeck_obj=pdk.Deck(layers = pdk.Layer("GridLayer",
                                                         pitch=45)
                                     )
                 )
+
+st.subheader('Distribuição dos preços dentro da faixa escolhida')
+
+st.plotly_chart( px.histogram(df_filtrado, x="preco", color="cat.semana",
+                   marginal="box",
+                   hover_data=df.columns).update_layout(xaxis_title='Preço dos Imóveis', yaxis_title='Contagem de imóveis'))
+
+
